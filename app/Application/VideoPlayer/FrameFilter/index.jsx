50a3d6095ee45ff7b8b21f7216807c7bf9551cf6
vladimir.muravyev <muravjovv1@gmail.com>
2016-02-02 19:20:32 +0300

    test dnd

--- app/Application/VideoPlayer/FrameFilter/index.jsx
+++ app/Application/VideoPlayer/FrameFilter/index.jsx
@@ -5,6 +5,22 @@ import style from "./style";
 export default class FrameFilter extends React.Component {
     constructor(options) {
         super(options);
+
+        this.state = {
+          isClick: false
+        };
+    }
+
+    handleOnDragStart(event) {
+        debugger
+    }
+
+    handleOnDagOver(event) {
+        console.info(arguments);
+    }
+
+    handleOnDragEnd(event) {
+
     }
 
     render() {
@@ -24,7 +40,37 @@ export default class FrameFilter extends React.Component {
         }
 
         return (
-            <div className={className} style={style}></div>
+            <div
+                className="b-filter_frame_container">
+                <div className={className} style={style}>
+                    <div className="b-filter_frame_controls">
+                        <div
+                            className="b-filter_frame_control b-filter_frame_controls--left_top"
+                            onDragStart={this.handleOnDragStart.bind(this)}
+                            onDragOver={this.handleOnDagOver.bind(this)}
+                            onDragEnd={this.handleOnDragEnd.bind(this)}>
+                        </div>
+                        <div
+                            className="b-filter_frame_control b-filter_frame_controls--right_top"
+                            onDragStart={this.handleOnDragStart.bind(this)}
+                            onDragOver={this.handleOnDagOver.bind(this)}
+                            onDragEnd={this.handleOnDragEnd.bind(this)}>
+                        </div>
+                        <div
+                            className="b-filter_frame_control b-filter_frame_controls--right_bottom"
+                            onDragStart={this.handleOnDragStart.bind(this)}
+                            onDragOver={this.handleOnDagOver.bind(this)}
+                            onDragEnd={this.handleOnDragEnd.bind(this)}>
+                        </div>
+                        <div
+                            className="b-filter_frame_control b-filter_frame_controls--left_bottom"
+                            onDragStart={this.handleOnDragStart.bind(this)}
+                            onDragOver={this.handleOnDagOver.bind(this)}
+                            onDragEnd={this.handleOnDragEnd.bind(this)}>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
